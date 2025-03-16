d7329a660a449ddbc3809f712aba3b7e03a71c45
basilisSam <basilis.samoladas@gmail.com>
2025-03-16 09:57:35 +0100

    swap with primary button

--- app/src/main/java/com/example/randomtraveller/login/LoginScreen.kt
+++ app/src/main/java/com/example/randomtraveller/login/LoginScreen.kt
@@ -8,15 +8,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.randomtraveller.R
+import com.example.randomtraveller.core.ui.PrimaryButton
 import com.example.randomtraveller.ui.theme.RandomTravellerTheme
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -42,9 +42,11 @@ fun LoginScreen(
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             Box(
                 modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.BottomCenter
             ) {
-                Column(modifier = modifier.padding(innerPadding)) {
+                Column(
+                    modifier = modifier.padding(innerPadding),
+                ) {
                     LoginButton()
                 }
             }
@@ -57,9 +59,10 @@ fun LoginButton() {
     val context = LocalContext.current
     val sigInInLauncher = getSigInInLauncher(context)
 
-    Button(onClick = { sigInInLauncher.launch(signInIntent) }) {
-        Text("Sign In")
-    }
+    PrimaryButton(
+        text = "Sign-in",
+        modifier = Modifier.padding(bottom = 20.dp)
+    ) { sigInInLauncher.launch(signInIntent) }
 }
 
 @Composable
@@ -76,7 +79,7 @@ private fun onSignInResult(
     result: FirebaseAuthUIAuthenticationResult,
 ) {
     if (result.resultCode != RESULT_OK) {
-        Toast.makeText(context, "Couldn't login ${result.idpResponse?.error}", Toast.LENGTH_LONG)
+        Toast.makeText(context, "Couldn't login ${result.idpResponse?.error?.message}", Toast.LENGTH_LONG)
             .show()
     }
 }
