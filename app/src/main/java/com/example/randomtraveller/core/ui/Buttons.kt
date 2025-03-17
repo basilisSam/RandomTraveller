ddd64f2a86eb5a83f36885ffd4a9a4c2e75e9cd1
basilisSam <basilis.samoladas@gmail.com>
2025-03-17 09:57:47 +0100

    Adjust primary button

--- app/src/main/java/com/example/randomtraveller/core/ui/Buttons.kt
+++ app/src/main/java/com/example/randomtraveller/core/ui/Buttons.kt
@@ -5,25 +5,53 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun PrimaryButton(text: String, modifier: Modifier = Modifier, onClick: () -> Unit) {
+fun PrimaryButton(
+    text: String,
+    isEnabled: Boolean = true,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
     Button(
         onClick = { onClick() },
         modifier = modifier
             .fillMaxWidth()
             .height(60.dp)
             .padding(horizontal = 16.dp),
-        shape = RoundedCornerShape(8.dp),
+        shape = RoundedCornerShape(48.dp),
+        enabled = isEnabled,
+        colors = ButtonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContentColor = Color.White,
+            disabledContainerColor = Color.LightGray
+        )
     ) {
         Text(
             text = text,
             fontSize = 16.sp
         )
     }
+}
+
+@Preview
+@Composable
+private fun DisabledPrimaryButtonPreview() {
+    PrimaryButton("Sign-in", false) { }
+}
+
+@Preview
+@Composable
+private fun EnabledPrimaryButtonPreview() {
+    PrimaryButton("Sign-in", true) { }
 }
