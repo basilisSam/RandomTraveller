f2462467ee1c3934f5e5311de0d572cdae1f5a0b
basilisSam <basilis.samoladas@gmail.com>
2025-03-18 20:16:22 +0100

    create DateUtils

---
+++ app/src/main/java/com/example/randomtraveller/core/utils/DateUtils.kt
@@ -0,0 +1,28 @@
+package com.example.randomtraveller.core.utils
+
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.TextStyle
+import java.util.Locale
+
+fun Long.toLocalDate(): LocalDate {
+    return Instant
+        .ofEpochMilli(this)
+        .atZone(ZoneId.systemDefault())
+        .toLocalDate()
+}
+
+fun LocalDate.toFormattedDateString(): String {
+    val dayOfWeek = this.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault())
+    val dayOfMonth = this.dayOfMonth
+    val year = this.year
+
+    val dayOfMonthSuffix = when (dayOfMonth) {
+        1, 21, 31 -> "st"
+        2, 22 -> "nd"
+        3, 23 -> "rd"
+        else -> "th"
+    }
+    return "$dayOfWeek, ${dayOfMonth}${dayOfMonthSuffix} of $year"
+}
