1de2734b9c2ba9f21fb9e24ae6c5b288be830cd6
basilisSam <basilis.samoladas@gmail.com>
2025-03-21 20:54:59 +0100

    make date picker a date ranger picker

--- app/src/main/java/com/example/randomtraveller/flights/ui/MyDatePicker.kt
+++ app/src/main/java/com/example/randomtraveller/flights/ui/MyDatePicker.kt
@@ -1,40 +1,56 @@
 package com.example.randomtraveller.flights.ui
 
-import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DateRangePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.example.randomtraveller.R
+import com.example.randomtraveller.core.utils.toLocalDate
+import java.time.LocalDate
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyDatePicker(
-    selectedDateInLong: Long? = null,
-    onDateSelected: (OnAction.OnDateSelected) -> Unit,
-    onDismiss: () -> Unit
+    selectedStartDate: Long? = null,
+    selectedEndDate: Long? = null,
+    onAction: (OnAction) -> Unit,
 ) {
-    val dateState = rememberDatePickerState(initialSelectedDateMillis = selectedDateInLong)
+    val dateState = rememberDateRangePickerState(
+        initialSelectedStartDateMillis = selectedStartDate,
+        initialSelectedEndDateMillis = selectedEndDate,
+        selectableDates = object : SelectableDates {
+            override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                return !utcTimeMillis.toLocalDate().isBefore(LocalDate.now())
+            }
+        }
+    )
     DatePickerDialog(
-        onDismissRequest = onDismiss,
+        onDismissRequest = { onAction(OnAction.OnDismissDatePicker) },
         confirmButton = {
             TextButton(onClick = {
-                onDateSelected(OnAction.OnDateSelected(dateState.selectedDateMillis))
-                onDismiss()
+                onAction(
+                    OnAction.OnDateRangeSelected(
+                        dateState.selectedStartDateMillis,
+                        dateState.selectedEndDateMillis
+                    )
+                )
+                onAction(OnAction.OnDismissDatePicker)
             }) {
                 Text(stringResource(R.string.select))
             }
         },
         dismissButton = {
-            TextButton(onClick = onDismiss) {
+            TextButton(onClick = { onAction(OnAction.OnDismissDatePicker) }) {
                 Text(stringResource(R.string.cancel))
             }
         }
     )
     {
-        DatePicker(state = dateState)
+        DateRangePicker(state = dateState)
     }
 }
