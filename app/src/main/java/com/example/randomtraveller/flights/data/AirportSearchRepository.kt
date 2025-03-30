6e37035e825cf91a2371b425b5fda03bfa0b59e8
basilisSam <basilis.samoladas@gmail.com>
2025-03-30 12:05:49 +0200

    remove unnecessary return statement

--- app/src/main/java/com/example/randomtraveller/flights/data/AirportSearchRepository.kt
+++ app/src/main/java/com/example/randomtraveller/flights/data/AirportSearchRepository.kt
@@ -4,21 +4,17 @@ import com.example.randomtraveller.flights.data.service.AirportData
 import com.example.randomtraveller.flights.data.service.AirportService
 import javax.inject.Inject
 
-class AirportSearchRepository
-    @Inject
-    constructor(
-        private val airportService: AirportService,
-    ) {
-        suspend fun getAirports(cityName: String): List<AirportData> {
-            return try {
-                val response = airportService.getAirports(cityName)
-                if (response.isSuccessful) {
-                    return response.body() ?: emptyList()
-                } else {
-                    emptyList()
-                }
-            } catch (e: Exception) {
+class AirportSearchRepository @Inject constructor(private val airportService: AirportService) {
+    suspend fun getAirports(cityName: String): List<AirportData> {
+        return try {
+            val response = airportService.getAirports(cityName)
+            if (response.isSuccessful) {
+                response.body() ?: emptyList()
+            } else {
                 emptyList()
             }
+        } catch (e: Exception) {
+            emptyList()
         }
     }
+}
