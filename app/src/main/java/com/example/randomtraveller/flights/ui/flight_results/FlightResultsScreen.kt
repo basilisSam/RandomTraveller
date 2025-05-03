8e2fe1a6f9299a00461395fb8f83e86188c1a251
basilisSam <basilis.samoladas@gmail.com>
2025-05-03 19:17:35 +0200

    add loading video when screen state is loading

--- app/src/main/java/com/example/randomtraveller/flights/ui/flight_results/FlightResultsScreen.kt
+++ app/src/main/java/com/example/randomtraveller/flights/ui/flight_results/FlightResultsScreen.kt
@@ -2,7 +2,11 @@ package com.example.randomtraveller.flights.ui.flight_results
 
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -15,15 +19,26 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player.REPEAT_MODE_ONE
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+import com.example.randomtraveller.R
 import com.example.randomtraveller.flights.ui.flight_results.components.FlightCard
 import com.example.randomtraveller.ui.theme.RandomTravellerTheme
 
@@ -67,32 +82,79 @@ private fun Content(
             )
         }
     ) { innerPadding ->
-        when (LocalConfiguration.current.orientation) {
-            Configuration.ORIENTATION_PORTRAIT -> {
-                LazyColumn(modifier = Modifier.padding(innerPadding)) {
-                    items(
-                        items = screenState.flights,
-                        key = { roundTripFlight -> roundTripFlight.id }) {
-                        FlightCard(roundTripFlight = it)
+        if (screenState.isLoading) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
+            ) {
+                LocalVideoPlayer(Modifier
+                    .height(200.dp)
+                    .width(150.dp))
+            }
+        } else {
+            when (LocalConfiguration.current.orientation) {
+                Configuration.ORIENTATION_PORTRAIT -> {
+                    LazyColumn(modifier = Modifier.padding(innerPadding)) {
+                        items(
+                            items = screenState.flights,
+                            key = { roundTripFlight -> roundTripFlight.id }) {
+                            FlightCard(roundTripFlight = it)
+                        }
                     }
                 }
-            }
 
-            else -> {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2),
-                    modifier = Modifier.padding(innerPadding)
-                ) {
-                    items(
-                        count = screenState.flights.size,
-                        key = { index -> screenState.flights[index].id }
-                    ) { index ->
-                        FlightCard(roundTripFlight = screenState.flights[index])
+                else -> {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        items(
+                            count = screenState.flights.size,
+                            key = { index -> screenState.flights[index].id }
+                        ) { index ->
+                            FlightCard(roundTripFlight = screenState.flights[index])
+                        }
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun LocalVideoPlayer(
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    val uri = remember {
+        "android.resource://${context.packageName}/${R.raw.random_traveller_loading}".toUri()
+    }
+
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
+            setMediaItem(MediaItem.fromUri(uri))
+            prepare()
+            playWhenReady = true
+            repeatMode = REPEAT_MODE_ONE
+        }
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            exoPlayer.release()
+        }
+    }
+
+    AndroidView(
+        factory = {
+            PlayerView(it).apply {
+                player = exoPlayer
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @PreviewScreenSizes
